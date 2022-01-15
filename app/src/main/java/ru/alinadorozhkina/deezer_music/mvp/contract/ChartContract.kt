9fc1da879c359a16cd86d1b0dc90e47c2b89b392
app-droid <alinadorozhkina91@mail.ru>
2022-01-15 15:43:26 +0300

    refactor:delete not usefull code

--- app/src/main/java/ru/alinadorozhkina/deezer_music/mvp/contract/ChartContract.kt
+++ app/src/main/java/ru/alinadorozhkina/deezer_music/mvp/contract/ChartContract.kt
@@ -1,10 +1,6 @@
 package ru.alinadorozhkina.deezer_music.mvp.contract
 
-import io.reactivex.Observable
-import io.reactivex.Single
 import ru.alinadorozhkina.deezer_music.mvp.model.entities.Chart
-import ru.alinadorozhkina.deezer_music.mvp.model.entities.Genre
-import ru.alinadorozhkina.deezer_music.mvp.model.entities.Track
 
 interface ChartContract {
 
@@ -12,11 +8,6 @@ interface ChartContract {
 
     interface Presenter: IPresenter<Chart, View> {
         fun getChartData()
-    }
-
-    interface Repository<T> {
-
-        fun getData(): Observable<T>
     }
 
     interface BaseClickListener
