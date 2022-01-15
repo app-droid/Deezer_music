370e893cd547bcff6ddf8b8ff31dabe28a097ac7
app-droid <alinadorozhkina91@mail.ru>
2022-01-15 15:34:31 +0300

    feat: add navigation to Player Fragment

--- app/src/main/java/ru/alinadorozhkina/deezer_music/mvp/ui/navigation/AndroidScreens.kt
+++ app/src/main/java/ru/alinadorozhkina/deezer_music/mvp/ui/navigation/AndroidScreens.kt
@@ -2,9 +2,11 @@ package ru.alinadorozhkina.deezer_music.mvp.ui.navigation
 
 import com.github.terrakok.cicerone.Screen
 import com.github.terrakok.cicerone.androidx.FragmentScreen
+import ru.alinadorozhkina.deezer_music.mvp.model.entities.Track
 import ru.alinadorozhkina.deezer_music.mvp.navigation.IScreens
 import ru.alinadorozhkina.deezer_music.mvp.ui.fragments.ChartFragment
 import ru.alinadorozhkina.deezer_music.mvp.ui.fragments.FavouritesFragment
+import ru.alinadorozhkina.deezer_music.mvp.ui.fragments.PlayerFragment
 import ru.alinadorozhkina.deezer_music.mvp.ui.fragments.RadioFragment
 
 class AndroidScreens : IScreens {
@@ -14,5 +16,7 @@ class AndroidScreens : IScreens {
     override fun radio(): Screen = FragmentScreen { RadioFragment.newInstance() }
 
     override fun favourites(): Screen = FragmentScreen { FavouritesFragment.newInstance() }
+
+    override fun player(track: Track): Screen = FragmentScreen { PlayerFragment.newInstance(track) }
 
 }
